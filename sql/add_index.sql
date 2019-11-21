f7167262c085952c15c68c4308ef5aceb3bea5a1
koiso <t-koiso@web-cte.co.jp>
2019-11-21 15:50:40 +0900

    DB インデックス追加

---
+++ sql/add_index.sql
@@ -0,0 +1,6 @@
+CREATE INDEX idx_main_term_id ON Center (main_term_id);
+CREATE INDEX idx_main_term_id ON ExplainIndex (main_term_id);
+CREATE INDEX idx_term_id ON MainTerm (term_id);
+CREATE INDEX idx_main_term_id ON Refer (main_term_id);
+CREATE INDEX idx_main_term_id ON SubTerm (main_term_id);
+CREATE INDEX idx_main_term_id ON Synonym (main_term_id);
